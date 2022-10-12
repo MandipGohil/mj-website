3189d34bbface33e07bd6b0a97a82a75012f2b84
Mandipsinh Jaydipsinh Gohil <mandipsinh046@gmail.com>
2022-10-11 23:57:04 -0700

    fix

--- index.php
+++ index.php
@@ -317,19 +317,19 @@
          <div class="twelve columns">
 
             <ul class="social-links">
-              <li><a href="https://www.facebook.com/mandip.gohil"><i class="fa fa-facebook"></i></a></li>
+              <!-- <li><a href="https://www.facebook.com/mandip.gohil"><i class="fa fa-facebook"></i></a></li>
               <li><a href="https://twitter.com/mandipgohil30"><i class="fa fa-twitter"></i></a></li>
               <li><a href="https://www.linkedin.com/in/mandipsinh-gohil"><i class="fa fa-linkedin"></i></a></li>
               <li><a href="https://www.instagram.com/_royalrajput_/"><i class="fa fa-instagram"></i></a></li>
-              <li><a href="#"><i class="fa fa-skype"></i></a></li>
-              <!--
+              <li><a href="#"><i class="fa fa-skype"></i></a></li> -->
+              
                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-               <li><a href="#"><i class="fa fa-skype"></i></a></li> -->
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
             </ul>
 
             <ul class="copyright">
